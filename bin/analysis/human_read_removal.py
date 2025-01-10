68e53e0a3dd943330699f1104d2d8576f2a97890
Sean <Sean.Donahue.5311@gmail.com>
2025-01-10 16:27:39 -0500

    Testing alternative implementation of unzipping and sanitizing

--- bin/analysis/human_read_removal.py
+++ bin/analysis/human_read_removal.py
@@ -19,19 +19,23 @@ READ_REMOVAL_COMMAND_TEMPLATE = [
 
 
 def unzip_input_file(input_file: Path, unzipped_dir) -> Path:
+    unzipped_path = Path(unzipped_dir) / input_file.stem if ".gz" in input_file.suffixes else Path(unzipped_dir) / input_file.name
     if ".gz" in input_file.suffixes:
-        unzip_command = f"gunzip -c {input_file} > {unzipped_dir}/{input_file.name}"
-        print(unzip_command)
+        unzipped_path = Path(unzipped_dir) / input_file.stem
+        unzip_command = f"gunzip -c {input_file} > {unzipped_path}"
         check_call(unzip_command, shell=True)
-        return unzipped_dir / Path(input_file.stem)
     else:
-        return input_file
+        copy_command = f"cp {input_file} {unzipped_path}"
+        check_call(copy_command, shell=True)
+
+    print(unzipped_path.exists())
+    return input_file
 
 def zip_output_file(output_file: Path):
     zip_command = f"gzip {output_file}"
     check_call(zip_command, shell=True)
 
-def single_file_human_read_remove(fastq_file_and_subdirs: Tuple[Path, Path, Path], threads: int):
+def single_file_human_read_remove(fastq_file_and_subdirs: Tuple[Path, Path], threads: int):
     """
     Run human read removal on a single fastq file
 
@@ -40,9 +44,8 @@ def single_file_human_read_remove(fastq_file_and_subdirs: Tuple[Path, Path, Path
     """
 
     input_path = fastq_file_and_subdirs[0]
-    unzipped_input_path = unzip_input_file(input_path, fastq_file_and_subdirs[2])
     output_path = fastq_file_and_subdirs[1] / fastq_file_and_subdirs[0].name
-    command = [piece.format(input_path=unzipped_input_path, out_path=output_path, threads=threads) for piece in READ_REMOVAL_COMMAND_TEMPLATE]
+    command = [piece.format(input_path=input_path, out_path=output_path, threads=threads) for piece in READ_REMOVAL_COMMAND_TEMPLATE]
     print("Running", " ".join(command))
     check_call(command, shell=True)
     zip_output_file(output_path)
@@ -58,13 +61,20 @@ def main(directory: Path, threads: int):
     unzipped_dir = Path("unzipped")
 
     for directory, files in fastq_files_by_directory.items():
-        subdir = out_dir / directory
         unzipped_subdir = unzipped_dir / directory
-        subdir.mkdir(exist_ok=True, parents=True)
         unzipped_subdir.mkdir(exist_ok=True, parents=True)
 
         for file in files:
-            single_file_human_read_remove((file, subdir, unzipped_subdir), threads)
+            unzip_input_file(file, unzipped_subdir)
+
+    unzipped_fastq_files_by_directory = collect_fastq_files_by_directory(unzipped_dir)
+
+    for directory, files in unzipped_fastq_files_by_directory.items():
+        subdir = out_dir / directory
+        subdir.mkdir(exist_ok=True, parents=True)
+
+        for file in files:
+            single_file_human_read_remove((file, subdir), threads)
 
 
 if __name__ == "__main__":
